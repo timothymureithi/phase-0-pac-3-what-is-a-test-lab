9ee4b1c71538a188e6d14eaf8c3376360e43dc3c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-13 22:51:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 37;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+describe("what-is-a-test", () => {
+    describe("Name", () => {
+      it('returns "Susan"', () => {
+        expect(name).toEqual("Susan");
+      });
+    });
+
+      
+        describe("Height", () => {
+          it("is less than 40", () => {
+            expect(height).toBeLessThan(40);
+          });
+        });
+})
+console.log(message);
+
+
